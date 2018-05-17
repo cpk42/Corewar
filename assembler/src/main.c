ba24e06a6a7539398cca6f1c0c1f74839337b5b5
Leonardo Tanenbaum-diaz <ltanenba@e1z1r6p18.42.us.org>
2018-05-17 00:26:51 -0700

    renamed a couple things. -Leo

--- assembler/src/main.c
+++ assembler/src/main.c
@@ -6,15 +6,15 @@
 /*   By: ltanenba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 23:46:46 by ltanenba          #+#    #+#             */
-/*   Updated: 2018/05/16 23:47:45 by ltanenba         ###   ########.fr       */
+/*   Updated: 2018/05/17 00:25:24 by ltanenba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "corewar.h"
+#include "asm.h"
 
 int			main(int argc, char **argv)
 {
-	ft_putstr("Hello Corewar!");
+	ft_putstr("Hello Asm!");
 	argc = 0;
 	argv = 0;
 	return (0);
