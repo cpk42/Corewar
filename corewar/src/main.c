4facb72e2302d4ad97c28b9ac8463821939f511b
Joshua Gelbard <jgelbard@e1z4r15p4.42.us.org>
2018-05-22 16:51:25 -0700

    Fix instruction argument check, make op_tab global

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ltanenba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 23:46:46 by ltanenba          #+#    #+#             */
-/*   Updated: 2018/05/21 23:44:00 by jgelbard         ###   ########.fr       */
+/*   Updated: 2018/05/22 16:50:34 by jgelbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,6 @@
 
 void		do_st(byte *ar, t_proc *ps, int idx);
 
-int			has_legal_argcodes(byte b, t_op *op)
-{
-	int		i;
-	int		argcode;
-
-	i = 0;
-	while (i < op->argc)
-	{
-		argcode = ARG_CODE(b, i);
-		if (!(argcode & op->legal_argcodes[i]))
-			return (0);
-		++i;
-	}
-	while (i < 4)
-	{
-		if (ARG_CODE(b, i))
-			return (0);
-		++i;
-	}
-	return (1);
-}
-
 void		read_instr(byte *ar, int idx, t_proc *ps)
 {
 	byte		opcode;
@@ -46,8 +24,8 @@ void		read_instr(byte *ar, int idx, t_proc *ps)
 
 	if((opcode = ar[0]) < 1 || opcode > MAX_OPCODE)
 		return ;
-	op = op_tab[opcode - 1];
-	if (op.has_codebyte == 1 && !has_legal_argcodes(ar[1], &op))
+	op = g_op_tab[opcode - 1];
+	if (op.has_codebyte == 1 && !has_legal_argtypes(ar[1], &op))
 		return ;
 	if (op.opcode == OP_ST)
 		do_st(ar, ps, idx);
@@ -56,10 +34,12 @@ void		read_instr(byte *ar, int idx, t_proc *ps)
 void		do_st(byte *ar, t_proc *ps, int idx)
 {
 	byte			*bytes;
+	t_arg_type		*argtypes;
 	unsigned long	ind_offset;
 
 	bytes = read_from_arena(ar, idx, 16, ps->pc);
-	if (ARG_CODE(bytes[1], 1) == T_REG)
+	argtypes = extract_argtypes(bytes[1]);
+	if (argtypes[1] == T_REG)
 	{
 		reg_reg_write(ps, bytes[3], bytes[2]);
 	}
@@ -80,7 +60,7 @@ int	main(void)
 	ps->regs[2] = 0xAABBCCDD;
 
 	mem[0] = 0x03; // OP_ST
-	mem[1] = 0x70; // argcodes: 10110000 == T_REG, T_DIR
+	mem[1] = 0x70; // argcodes: 10110000 == T_REG, T_IND
 	mem[2] = 0x02; // reg == r2
 	mem[3] = 0x00; // ind offset = 0 ...
 	mem[4] = 0x0a; // ... + 10
