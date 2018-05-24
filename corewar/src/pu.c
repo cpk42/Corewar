332b2addb09d2aeef6b9236d44e2343e00a8e1c9
Maksym Max Moros <Maksym.Moros@gmail.com>
2018-05-23 21:26:39 -0700

    understood incoming area/vm structures

--- corewar/src/pu.c
+++ corewar/src/pu.c
@@ -6,16 +6,39 @@
 /*   By: mmoros <mmoros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 20:21:33 by mmoros            #+#    #+#             */
-/*   Updated: 2018/05/23 21:07:55 by mmoros           ###   ########.fr       */
+/*   Updated: 2018/05/23 21:25:49 by mmoros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	processor(byte *ar, t_proc *procs)
+static int	alive(t_proc *procs, int *d_minus)
+{
+	int		life_count;
+
+	life_count = 0;
+	while (procs)
+	{
+		if (procs->lcount > 0)
+		{
+			life_count += procs->lcount;
+			procs->lcount = 0;
+			
+		}
+		else
+			del_proc(procs);
+		procs = procs->next;
+	}
+
+}
+
+void		processor(byte *ar, t_proc *procs)
 {
 	int		d_minus;
 
 	d_minus = CYCLE_TO_DIE;
-	
+	while (alive(procs, &d_minus))
+	{
+		
+	}
 }
