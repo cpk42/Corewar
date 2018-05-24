440f0247aa7afe45aa8b636cf8232a988f0df4cd
Maksym Max Moros <Maksym.Moros@gmail.com>
2018-05-24 14:21:32 -0700

    Need to restructure plan on alive checks. Gym break.

--- corewar/src/pu.c
+++ corewar/src/pu.c
@@ -6,7 +6,7 @@
 /*   By: mmoros <mmoros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 20:21:33 by mmoros            #+#    #+#             */
-/*   Updated: 2018/05/23 21:25:49 by mmoros           ###   ########.fr       */
+/*   Updated: 2018/05/24 14:20:32 by mmoros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,34 @@
 
 static int	alive(t_proc *procs, int *d_minus)
 {
-	int		life_count;
+	int		pulse_count;
 
-	life_count = 0;
+	if (g_vm.cycle < g_vm.dminus)
+		return (1);
+	pulse_count = 0;
 	while (procs)
 	{
 		if (procs->lcount > 0)
 		{
-			life_count += procs->lcount;
-			procs->lcount = 0;
-			
+			pulse_count++;
+			procs->lcount = 0;	
 		}
 		else
 			del_proc(procs);
 		procs = procs->next;
 	}
+	g_vm.alive_checks++;
+	if (pulse_count > NBR_LIVE || g_vm.alive_checks == MAX_CHECKS)
+	{
+		g_vm.alive_checks %= MAX_CHECKS;
+		
+	}
 
 }
 
-void		processor(byte *ar, t_proc *procs)
+void		processor()
 {
-	int		d_minus;
-
-	d_minus = CYCLE_TO_DIE;
-	while (alive(procs, &d_minus))
+	while (alive())
 	{
 		
 	}
--- corewar/src/vm_init.c
+++ corewar/src/vm_init.c
@@ -6,7 +6,7 @@
 /*   By: ltanenba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 20:07:16 by ltanenba          #+#    #+#             */
-/*   Updated: 2018/05/23 21:25:42 by ltanenba         ###   ########.fr       */
+/*   Updated: 2018/05/24 13:47:32 by mmoros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ void		initialize_vm(int player_num)
 	g_vm.cycle = 0;
 	g_vm.dminus = 0;
 	g_arena = &g_ARENA_MEMORY[0];
+	g_vm.alive_checks = 0;
 }
