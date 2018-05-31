0ba11e4637f35251aa6e2575b6ad554e08201831
Leonardo Tanenbaum-diaz <ltanenba@e1z1r6p18.42.us.org>
2018-05-30 22:14:10 -0700

    beggining work on first_pass

--- assembler/src/first_pass.c
+++ assembler/src/first_pass.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   first_pass.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ltanenba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/16 23:46:46 by ltanenba          #+#    #+#             */
-/*   Updated: 2018/05/30 21:15:37 by ltanenba         ###   ########.fr       */
+/*   Created: 2018/05/30 21:15:46 by ltanenba          #+#    #+#             */
+/*   Updated: 2018/05/30 21:15:58 by ltanenba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int			main(int argc, char **argv)
-{
-	int			i;
 
-	i = 0;
-	if (argc > 1)
-	{
-		while (++i > argc)
-			first_pass(argv[i]);
-	}
-	ft_putstr("Hello Asm!");
-	argc = 0;
-	argv = 0;
-	return (0);
-}
